b7cd80be5398266cd39654dde137f40d935b4954
Stive627 <fossistive627@gmail.com>
2025-05-16 12:30:55 +0530

    last commit for this fixed bottom

--- Components/login/Login.js
+++ Components/login/Login.js
@@ -44,7 +44,7 @@ function Login({getAuthenticated}) {
         {status.success && <Confetti width={width} height={height}/>}
         </div>
       </div>
-      <div className='absolute bottom-0  w-full'>
+      <div className='fixed z-30 bottom-0  w-full'>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#d60f12" fillOpacity="1" d="M0,128L60,133.3C120,139,240,149,360,133.3C480,117,600,75,720,96C840,117,960,203,1080,229.3C1200,256,1320,224,1380,208L1440,192L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
       </div>
     </div>
