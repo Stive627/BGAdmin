73c692dd125e9989c0598869d46d356677063392
Stive627 <fossistive627@gmail.com>
2025-05-16 09:16:02 +0530

    let try again

--- Components/login/Login.js
+++ Components/login/Login.js
@@ -24,7 +24,7 @@ function Login({getAuthenticated}) {
     .finally(()=>setPending(false))
   }
   return (
-    <div className='relative w-screen h-screen flex  justify-center'>
+    <div className='relative w-screen h-screen flex justify-center'>
       <div className={`w-5/6 lg:w-1/5 mt-20`}>
         <div className=' flex justify-center'>
           <div className='flex flex-row items-center gap-4 '>
@@ -40,9 +40,9 @@ function Login({getAuthenticated}) {
           <div className=' flex justify-center'><button disabled={!validForm} style={{backgroundColor:validForm?(pending?'rgba(214, 15, 18, 0.4)':'rgba(214, 15, 18, 1)'):'rgba(214, 15, 18, 0.4)'}} type='submit' className=' text-white w-1/3 rounded-md py-1 px-2'>Login</button></div>
         </form>
         <p className=' text-center mt-7'>Forgot password? <span><button style={{color:'rgba(214, 15, 18, 1)'}} className=' underline'>Recover</button></span></p>
-      </div>
       {status.success && <Confetti width={width} height={height}/>}
-      <div className=' absolute bottom-0 w-screen'>
+      </div>
+      <div className='absolute bottom-0  w-full'>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#d60f12" fillOpacity="1" d="M0,128L60,133.3C120,139,240,149,360,133.3C480,117,600,75,720,96C840,117,960,203,1080,229.3C1200,256,1320,224,1380,208L1440,192L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
       </div>
     </div>
